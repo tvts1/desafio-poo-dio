0e64a3e235a4a1454bb7d127e98a9d4b55148843
TassioTristao <tassio.tristao05@gmail.com>
2025-08-12 17:18:04 -0300

    Aprimora classe Conteudo: torna XP_PADRAO constante pública, adiciona construtor para título e descrição

---
+++ src/domain/Conteudo.java
@@ -0,0 +1,43 @@
+package domain;
+
+public abstract class Conteudo {
+
+    public static final double XP_PADRAO = 10d;
+
+    private String titulo;
+    private String descricao;
+
+    public Conteudo() {
+    }
+
+    public Conteudo(String titulo, String descricao) {
+        this.titulo = titulo;
+        this.descricao = descricao;
+    }
+
+    public abstract double calcularXp();
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    @Override
+    public String toString() {
+        return "Conteudo{" +
+                "titulo='" + titulo + '\'' +
+                ", descricao='" + descricao + '\'' +
+                '}';
+    }
+}
